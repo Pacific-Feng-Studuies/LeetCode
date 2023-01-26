d30246bd22dc9387c38fd333b15a8bcb560aea06
Min-Feng, Hsieh <69132764+Pacific-Feng@users.noreply.github.com>
2023-01-26 12:15:53 +0800

    Update main.cpp

--- 491. Non-decreasing Subsequences/personal compile/main.cpp
+++ 491. Non-decreasing Subsequences/personal compile/main.cpp
@@ -1,1 +1,77 @@
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) ;
+    
+    vector<int> nums;
+};
+
+void backtrace(vector<int>& nums, vector<int>& sequence, set<vector<int>>& result, int index)
+{   
+    if(index == nums.size())
+    {
+        if(sequence.size() >= 2)
+        {
+            result.insert(sequence);
+        }
+        return;
+    }
+    
+    if(sequence.empty() == true ||  sequence.back() <= nums[index])
+    {
+        sequence.emplace_back(nums[index]);
+        backtrace(nums, sequence, result, index+1);
+        sequence.pop_back();
+    }
+    backtrace(nums, sequence, result, index+1);
+}
+
+vector<vector<int>> Solution::findSubsequences(vector<int>& nums) 
+{
+    set<vector<int>> result;
+    vector<int> sequence;
+
+    backtrace(nums, sequence, result, 0);
+
+    return vector<vector<int> >(result.begin(), result.end());
+}
+
+int main()
+{
+    Solution solution;
+
+    vector<int>nums{4, 6, 7, 7, 1};
+    solution.nums = nums;
+
+    vector<vector<int> > result;
+    result = solution.findSubsequences(solution.nums);
+
+    cout << "[";
+
+    for(int i = 0 ; i < result.size(); i++)
+    {
+        cout << "[" ;
+        for(int j = 0 ; j < result[i].size() ; j++)
+        {
+            cout << result[i][j] ;
+
+            if(j != result[i].size()-1)
+            {
+                cout << " ," ;
+            }
+        }
+        cout << "]" ;
+
+        if(i == result.size()-1)
+        {
+            cout << "]" << endl;
+        }
+    }
+}
