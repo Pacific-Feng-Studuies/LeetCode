a4d78884d78977ef1b5b75aba33358b4869fdffa
Min-Feng, Hsieh <69132764+Pacific-Feng@users.noreply.github.com>
2023-01-30 19:54:30 +0800

    Update main.cpp

--- 787. Cheapest Flights Within K Stops/BFS Way/leetcode compile/main.cpp
+++ 787. Cheapest Flights Within K Stops/BFS Way/leetcode compile/main.cpp
@@ -1,1 +1,41 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>> > dist(n);    // dist[src] -> (dst, distance)
+    vector<int> distRecord(n, INT_MAX);         // minimun distance to each node (node, min distance)
+    queue<pair<int, int>> Q;                    // to record the BFS process
+    int stop = 0;                               // the number of stops
 
+    // To construct the dist
+    for(auto &p :flights)
+    {
+        dist[p[0]].emplace_back(make_pair(p[1],p[2]));
+    }
+
+    // To do BFS
+    Q.push(make_pair(src, 0));
+
+    while( stop <= k && !Q.empty() )
+    {
+        int qSize = Q.size();
+
+        while(qSize--)  // if qSize == 0 ,then jump out the loop
+        {
+            auto [node, minDist] = Q.front(); // only for c++17 up
+            Q.pop();
+
+            for(auto &[neighbor, cost] : dist[node])
+            {
+                if(minDist+cost < distRecord[neighbor])
+                {
+                    distRecord[neighbor] = minDist+cost;
+                    Q.push({neighbor, distRecord[neighbor]});
+                }
+            }
+        }
+        stop++;
+    }
+
+    return distRecord[dst] == INT_MAX? -1 : distRecord[dst];
+    }
+};
