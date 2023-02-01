cd8824b677c139041a750c2d677bad45df85ffa6
Min-Feng, Hsieh <69132764+Pacific-Feng@users.noreply.github.com>
2023-02-01 13:23:41 +0800

    Update main.cpp

--- 787. Cheapest Flights Within K Stops/Bellman Ford/leetcode compile/main.cpp
+++ 787. Cheapest Flights Within K Stops/Bellman Ford/leetcode compile/main.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> distRecord(n, INT_MAX);         // minimun distance to each node (node, min distance)
+    
+    distRecord[src] = 0;
 
+    for(int i = 0 ; i <= k ; i++)
+    {
+        vector<int> tmp(distRecord);    // to copy the distRecord
+
+        for(auto& p : flights)
+        {
+            if(distRecord[p[0]] != INT_MAX)
+            {
+                tmp[p[1]] = min(distRecord[p[0]]+p[2], tmp[p[1]]);  // use this net or the prev added weight
+            }
+        }
+
+        distRecord = tmp;
+    }
+
+    return distRecord[dst] == INT_MAX? -1 : distRecord[dst];
+    }
+};
