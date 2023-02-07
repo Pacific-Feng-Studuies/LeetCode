7c3673f128b02ac398151476379fa7c03e8f369b
Min-Feng, Hsieh <69132764+Pacific-Feng@users.noreply.github.com>
2023-02-07 14:22:19 +0800

    Add files via upload

---
+++ 904. Fruit Into Baskets/personal compile/execution.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make
+./Fruit_Into_Baskets
---
+++ 904. Fruit Into Baskets/personal compile/main.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits);
+
+    vector<int> fruits;
+};
+
+int Solution::totalFruit(vector<int>& fruits)
+{
+    unordered_map <int, int> basket;    // just for search
+
+    int maxFruit = 0;       // The maximun fruit current picked
+    int leftIndex = 0;      // The starting index current do
+
+    for(int i = 0 ; i < fruits.size() ; i++)
+    {
+        basket[fruits[i]]++;
+
+        while(basket.size()>2)
+        {
+            if(basket[fruits[leftIndex]] == 1)    basket.erase(fruits[leftIndex]);  // after -1, the result will be 1 -> so can erase
+            else                                  basket[fruits[leftIndex]]--;      // reduce the number of the fruit
+
+            leftIndex++;
+        }
+
+        maxFruit = max(maxFruit, i-leftIndex+1);
+    }
+
+    return maxFruit;
+}
+
+int main()
+{
+    Solution solution;
+
+    solution.fruits = {1,2,3,2,2};
+    int result = solution.totalFruit(solution.fruits);
+
+    cout << "The result equals to " << result << endl;
+
+    return 0;
+}
